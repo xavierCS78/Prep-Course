a1279d1e59dee22d96cc4550dbfaea6f06f33c1b
unknown <javiercarosoa@gmail.com>
2022-07-15 10:34:30 -0500

    first change to hw2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -4,7 +4,10 @@ function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
-  // Tu código:
+  if ( x > y){
+    return x;
+  }return y;
+
 }
 
 function mayoriaDeEdad(edad) {
